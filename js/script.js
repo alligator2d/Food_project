17afac6ebff298509e733dfb0129e602f535fa41
Oleg <alligator2d@rambler.ru>
2022-12-08 10:45:32 +0300

    add slider

--- js/script.js
+++ js/script.js
@@ -196,83 +196,138 @@ window.addEventListener('DOMContentLoaded', () => {
     //         data.data.forEach(({img, alt, title, descr, price}) => {
     //             new MenuCard(img, alt, title, descr, price, '.menu .container').render();
     //         })
-            
-            
-            //Forms
-            const forms = document.querySelectorAll('form');
-            const message = {
-                loading: 'img/form/spinner.svg', success: 'Thanks! We will contact you soon =)', failure: 'Fatal Error',
-            };
 
-            forms.forEach((item) => {
-                bindPostData(item);
-            });
 
-            const postData = async (url, data) => {
-                const res = await fetch(url, {
-                    method: 'POST',
-                    headers: {'Content-Type': 'application/json'},
-                    body: data,
-                });
-                return await res.json();
-            }
+    //Forms
+    const forms = document.querySelectorAll('form');
+    const message = {
+        loading: 'img/form/spinner.svg', success: 'Thanks! We will contact you soon =)', failure: 'Fatal Error',
+    };
 
-            function bindPostData(form) {
-                form.addEventListener('submit', (e) => {
-                    e.preventDefault();
+    forms.forEach((item) => {
+        bindPostData(item);
+    });
 
-                    const statusMessage = document.createElement('img');
-                    statusMessage.src = message.loading;
-                    statusMessage.style.cssText = `
+    const postData = async (url, data) => {
+        const res = await fetch(url, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: data,
+        });
+        return await res.json();
+    }
+
+    function bindPostData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('img');
+            statusMessage.src = message.loading;
+            statusMessage.style.cssText = `
       display: block;
       margin: 0 auto;
       `;
 
-                    form.insertAdjacentElement('afterend', statusMessage);
+            form.insertAdjacentElement('afterend', statusMessage);
 
-                    const formData = new FormData(form);
+            const formData = new FormData(form);
 
-                    const json = JSON.stringify(Object.fromEntries(formData.entries()));
+            const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-                    postData('http://localhost:3000/requests', json)
-                        .then((data) => {
-                            console.log(data);
-                            showThanksModal(message.success);
-                            statusMessage.remove();
-                        })
-                        .catch(() => {
-                            showThanksModal(message.failure);
-                        })
-                        .finally(() => {
-                            form.reset();
-                        });
+            postData('http://localhost:3000/requests', json)
+                .then((data) => {
+                    console.log(data);
+                    showThanksModal(message.success);
+                    statusMessage.remove();
+                })
+                .catch(() => {
+                    showThanksModal(message.failure);
+                })
+                .finally(() => {
+                    form.reset();
                 });
-            }
+        });
+    }
 
-            function showThanksModal(message) {
-                const prevModalDialog = document.querySelector('.modal__dialog');
+    function showThanksModal(message) {
+        const prevModalDialog = document.querySelector('.modal__dialog');
 
-                prevModalDialog.classList.add('hide');
-                openModal();
+        prevModalDialog.classList.add('hide');
+        openModal();
 
-                const thanksModal = document.createElement('div');
-                thanksModal.classList.add('modal__dialog');
-                thanksModal.innerHTML = `
+        const thanksModal = document.createElement('div');
+        thanksModal.classList.add('modal__dialog');
+        thanksModal.innerHTML = `
             <div class="modal__content">
                 <div class="modal__close" data-close>×</div>
                 <div class="modal__title">${message}</div>
             </div>
         `;
-                document.querySelector('.modal').append(thanksModal);
-                setTimeout(() => {
-                    thanksModal.remove();
-                    prevModalDialog.classList.add('show');
-                    prevModalDialog.classList.remove('hide');
-                    closeModal();
-                }, 4000);
-            }
+        document.querySelector('.modal').append(thanksModal);
+        setTimeout(() => {
+            thanksModal.remove();
+            prevModalDialog.classList.add('show');
+            prevModalDialog.classList.remove('hide');
+            closeModal();
+        }, 4000);
+    }
 
-            fetch(' http://localhost:3000/menu')
-                .then(data => data.json())
-                .then(res => console.log(res))
-        });
+    fetch(' http://localhost:3000/menu')
+        .then(data => data.json())
+        .then(res => console.log(res))
+
+//slider
+
+    const images = document.querySelectorAll('.offer__slide');
+    const next = document.querySelector('.offer__slider-next');
+    const prev = document.querySelector('.offer__slider-prev');
+    let currentId = document.querySelector('#current');
+    let count = 0;
+
+    next.addEventListener('click', nextImg);
+    prev.addEventListener('click', prevImg);
+
+    hideImg(images, 'hide');
+    resetSlider();
+
+    function hideImg(arr, hideClass) {
+        arr.forEach(x => x.classList.add(hideClass));
+    }
+
+    function resetSlider() {
+        count = 0;
+        currentId.innerHTML = `01`;
+        images[count].classList.remove('hide');
+
+    }
+
+    function nextImg() {
+        count++;
+        hideImg(images, 'hide');
+        if (!images[count]) {
+            resetSlider();
+        } else {
+            images[count].classList.remove('hide');
+            currentId.innerHTML = `0${count + 1}`;
+        }
+
+    }
+
+
+    function prevImg() {
+        count--;
+        hideImg(images, 'hide');
+        if (!images[count]) {
+            resetSlider();
+
+        } else {
+            images[count].classList.remove('hide');
+            currentId.innerHTML = `0${count + 1}`;
+
+        }
+        if (count < 1) {
+            count = images.length;
+        }
+    }
+
+});
